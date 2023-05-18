058bbbf4a5c1d8533e7667834938617f189979d2
Foogg5776 <vcoj05508@gmail.com>
2023-05-18 16:59:46 +1000

    fdfdf

--- assets/index.js
+++ assets/index.js
@@ -3,33 +3,37 @@ let massobject = [
       "userId": 1,
       "id": 1,
       "title": "sunt",
-      "body": "quia et suscipit"
+      "body": "quia et suscipit",
+      "img": "./assets/img/metall.jpg"
     },
     {
       "userId": 1,
       "id": 2,
       "title": "qui est esse",
-      "body": "est rerum tempore "
+      "body": "est rerum tempore ",
+      "img": "./assets/img/metall.jpg"
     },
     {
       "userId": 1,
       "id": 3,
       "title": "ea molestias",
-      "body": "occaecati omnis eligendi aut "
+      "body": "occaecati omnis eligendi aut ",
+      "img": "./assets/img/metall.jpg"
     },
     {
       "userId": 1,
       "id": 4,
       "title": "eum et ",
-      "body": "ullam et "
+      "body": "ullam et ",
+      "img": "./assets/img/metall.jpg"
     },
 
   ]
 
 
 
-let object = document.querySelector(".object")
+let object = document.querySelector(".object__list")
 for (let i = 0; i < massobject.length; i++) {
-    object.insertAdjacentHTML('afterbegin', `<div class="object__card"><img src="assets/img/vl.jpg" alt="" class="card__img"><div class="card__link"><a href="" class="link__obj">Lorem ipsum dolor sit amet consectetur.</a></div></div>`)
+    object.insertAdjacentHTML('afterbegin', `<div class="object__card"><img src="${massobject[i].img}" alt="" class="card__img"><div class="card__link"><a href="" class="link__obj">${massobject[i].title}.</a></div></div>`)
     console.log(massobject[i].img);
 }
